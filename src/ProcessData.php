9aab458037b8ca682ba5b5677c8d9288cb500f73
isyel <isykaal4saviour@gmail.com>
2018-12-20 09:54:25 +0100

    merger of process data file

--- src/ProcessData.php
+++ src/ProcessData.php
@@ -16,15 +16,9 @@ class ProcessData {
 
     function pullDataFromGalooli()
     {
-<<<<<<< HEAD:src/ProcessData.php
-        echo "Pulling Data from galooli...<br/>";
-        // $this->apiURL = "http://tqworksng.com/api/Items?SearchKeyword=cab&PageNumber=0&PageSize=20";
-        $this->apiURL = "https://sdk.galooli-systems.com/galooliSDKService.svc/json/Assets_Report?userName=matrixvtrack&password=matv123?&requestedPropertiesStr=ac.status,u.id,u.name,ac.latitude,ac.longitude,ac.distance_[km],ac.engine_hours_[num]&lastGmtUpdateTime=2000-01-01%202000:00:00";
-=======
         echo "Pulling Data from galooli...<br/>";;
         $this->apiURL = "https://sdk.galooli-systems.com/galooliSDKService.svc/json/Assets_Report?userName=matrixvtrack&password=matv123?&requestedPropertiesStr=ac.status,u.id,u.name,ac.latitude,ac.longitude,ac.distance_[km],ac.engine_hours_[num]&lastGmtUpdateTime=2000-01-01 2000:00:00";
         
->>>>>>> 87f33515bc782d401f3a03c9a9f11b41319449c2:src/ProcessData.php
         $get_data = $this->_apiService->callAPI('GET', $this->apiURL, false, 'galooli');
         $response = json_decode($get_data, true);
         echo 'Response: '.$get_data."<br/>";;
