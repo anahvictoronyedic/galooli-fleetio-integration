d5ac9a11bd55984548302fbae42db8374171103c
Anah Victor <anahvictoronyedic@gmail.com>
2018-12-20 16:02:30 +0100

    Designed the cron logic code and other essentials

---
+++ src/utils.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once 'db.php';
+
+function configuration($name,$value=false){
+	global $db_server;
+	$name=mysqli_real_escape_string($db_server,$name);
+	if($value !== false){
+		$value=mysqli_real_escape_string($db_server,$value);
+		$query = "REPLACE INTO configuration(name,value) VALUES ('$name',".(is_null($value) ? "NULL" : "'$value'" ).")";
+		return mysqli_query($db_server,$query)!==false;
+	}
+	$query = "select * from configuration where name = '$name'";
+	$result = mysqli_query($db_server,$query);
+	if($result){
+		if( $row = mysqli_fetch_array($result) ){
+			return $row['value'];
+		}
+	}
+	return false;
+}
