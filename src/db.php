92c60e7f4f1ce15c7b2d7b953647c5e6a29f55c4
Anah Victor <anahvictoronyedic@gmail.com>
2018-12-29 16:13:53 +0100

    updated inserver db info

--- src/db.php
+++ src/db.php
@@ -5,9 +5,9 @@ require_once 'config.php';
 error_reporting( DEBUG_MODE ? E_ALL & ~E_DEPRECATED & ~E_NOTICE : 0 );
 
 $db_hostname = "localhost";
-$db_username = IN_SERVER ? "" :'root';
-$db_password = IN_SERVER ? "":'';
-$db_database = IN_SERVER ? "" : "galoolifleetio"; 
+$db_username = IN_SERVER ? "ecagon" :'root';
+$db_password = IN_SERVER ? "myskynet123sqldb":'';
+$db_database = IN_SERVER ? "galoolifleetio" : "galoolifleetio"; 
 
 $db_server=mysqli_connect($db_hostname,$db_username,$db_password); //connection is established here
 if(!$db_server)
